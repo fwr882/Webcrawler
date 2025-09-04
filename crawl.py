dd3a407cb878389136ad71748eee9b7f33a6048b
Fahim <fahimrahman2000@gmail.com>
2025-09-03 23:01:13 -0500

    Added GetHtml and crawling functionality

--- crawl.py
+++ crawl.py
@@ -1,5 +1,9 @@
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
+import requests
+from requests.exceptions import RequestException, HTTPError
+
+
 
 
 
@@ -86,3 +90,75 @@ def extract_page_data(html, page_url):
             "outgoing_links": [],
             "image_urls": []
         }
+
+
+def get_html(url):
+        resp = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"}, timeout=10)
+        resp.raise_for_status()  # Raise an error for bad status codes
+        
+        ct = resp.headers.get('Content-Type', '').lower()
+        
+        if "text/html" in ct:
+            return resp.text
+        raise ValueError(f"non-HTML content type: {ct}")
+    
+        
+def crawl_page(base_url, current_url=None, page_data=None, fail_data=None):
+    if current_url is None:
+        current_url = base_url
+    
+    
+    if page_data is None:
+        page_data = {}
+
+    #this dictionary will hold all the attempted but FAILED crawls so it doesn't attempt to crawl them again
+    if fail_data is None:
+        fail_data = set()
+    
+    #check if the current url is in the same domain as the base url
+    if urlparse(base_url).netloc != urlparse(current_url).netloc:
+        return
+    
+    
+    
+    normaled_url = normalize_url(current_url)
+    if normaled_url in fail_data:
+        print(f"skipping {current_url}, previously failed")
+        return
+    if normaled_url in page_data:
+        print(f"skipping {current_url}, already crawled")
+        return
+    
+    
+    print(f"crawling: {current_url}...")
+    try:
+        html = get_html(current_url)
+    except Exception as e:
+        print(f"Error fetching {current_url}: {str(e)}")
+        fail_data.add(normaled_url)
+        return
+   
+    #if everything went well extract and store the page data
+    
+    rich_data = extract_page_data(html, current_url)
+    page_data[normaled_url] = rich_data
+    
+    #Get all the Urls on the page and crawl them too
+    urls_on_page = get_urls_from_html(html, base_url)
+    for new_url in urls_on_page:
+        crawl_page(base_url, new_url, page_data, fail_data)
+    return page_data
+
+          
+            
+            
+         
+        
+        
+        
+    
+    
+    
+        
+        
+    
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+# python
+import sys
+from crawl import crawl_page
+
 def main():
-    print("Hello from webcrawler!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
 
-
+    base_url = sys.argv[1]
+    print(f"starting crawl of: {base_url}")
+    
+    
+    try:
+        page_data = crawl_page(base_url)
+        print("\n---Collected Data---\n")
+        print(f"Number of pages found: {len(page_data)}\n")
+        
+        for url, data in page_data.items():
+            print(f"URL: {url}")
+            print(f" header: {data['h1']}")
+            
+        
+        sys.exit(0)
+    except Exception as e:
+        print(f"Error fetching HTML: {str(e)}")
+        sys.exit(1)
+    
 if __name__ == "__main__":
     main()
