a8aed6b9c8e00504086c43f4aa7279ff02f601ca
Fahim <fahimrahman2000@gmail.com>
2025-09-04 22:52:31 -0500

    added csv building functionality

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 import sys
 import crawl
 import asyncio  
+import csv_report
+
 
 async def main_async():
     url = sys.argv[1]
     max_concurrency = int(sys.argv[2])
     max_pages = int(sys.argv[3]) 
     page_data = await crawl.crawl_site_async(url, max_concurrency, max_pages)
-    for data in page_data.values():
-        if data is None:
-            continue
-        print(data["url"], data["h1"])       
-        
+    csv_report.write_csv_report(page_data)        
     
     
     
